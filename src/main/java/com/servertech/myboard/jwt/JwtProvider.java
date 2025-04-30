26e67065bfc07385f3d08733e37ce6bda4564e3c
fbehddn <tom0611@naver.com>
2025-05-01 00:10:10 +0900

    feat: building a jwt env

---
+++ src/main/java/com/servertech/myboard/jwt/JwtProvider.java
@@ -0,0 +1,76 @@
+package com.servertech.myboard.jwt;
+
+import static io.jsonwebtoken.Header.JWT_TYPE;
+import static io.jsonwebtoken.Header.TYPE;
+import static io.jsonwebtoken.SignatureAlgorithm.HS256;
+
+import com.servertech.myboard.user.application.service.CustomUserDetailService;
+import io.jsonwebtoken.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class JwtProvider {
+	private final JwtProperties jwtProperties;
+	private final CustomUserDetailService userDetailsService;
+	private final static String HEADER_AUTHORIZATION = "Authorization";
+	private final static String TOKEN_PREFIX = "Bearer ";
+
+	public String generateToken(Long id) {
+		Date now = new Date();
+		return makeToken(new Date(now.getTime() + Duration.ofHours(1).toMillis()), id);
+	}
+
+	private String makeToken(Date expiry, Long id) {
+		Date now = new Date();
+
+		return Jwts.builder()
+			.setHeaderParam(TYPE, JWT_TYPE)
+			.setIssuer(jwtProperties.getIssuer())
+			.setIssuedAt(now)
+			.setExpiration(expiry)
+			.setSubject(id.toString())
+			.signWith(HS256, jwtProperties.getSecretKey())
+			.compact();
+	}
+
+	public String resolveToken(HttpServletRequest request) {
+		String bearerToken = request.getHeader(HEADER_AUTHORIZATION);
+		if (bearerToken != null && bearerToken.startsWith(TOKEN_PREFIX)) {
+			return bearerToken.substring(TOKEN_PREFIX.length());
+		}
+		return null;
+	}
+
+	public boolean validateToken(String token) {
+		if (token == null) {
+			return false;
+		}
+
+		Jwts.parser()
+			.setSigningKey(jwtProperties.getSecretKey())
+			.parseClaimsJws(token);
+		return true;
+
+	}
+
+	public Authentication getAuthentication(String token) {
+		UserDetails userDetails = userDetailsService.loadUserByUsername(getClaims(token).getSubject());
+		return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+	}
+
+	private Claims getClaims(String token) {
+		return Jwts.parser()
+			.setSigningKey(jwtProperties.getSecretKey())
+			.parseClaimsJws(token)
+			.getBody();
+	}
+}
