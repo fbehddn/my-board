5914bc0ace241b9308e5a39135313f30fecb682f
fbehddn <tom0611@naver.com>
2025-05-01 16:07:29 +0900

    chore: swagger config and auto fill field

--- src/main/java/com/servertech/myboard/user/application/dto/request/UserCreateRequest.java
+++ src/main/java/com/servertech/myboard/user/application/dto/request/UserCreateRequest.java
@@ -1,8 +1,15 @@
 package com.servertech.myboard.user.application.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record UserCreateRequest(
+	@Schema(description = "사용자 이메일", example = "email@google.com")
 	String email,
+
+	@Schema(description = "사용자 비밀번호", example = "password1234")
 	String password,
+
+	@Schema(description = "사용자 이름", example = "Cole Palmer")
 	String username
 ) {
 }
