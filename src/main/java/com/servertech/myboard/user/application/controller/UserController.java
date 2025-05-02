2b217421b36d1aac68f3aad8548a2bad8811b91d
fbehddn <tom0611@naver.com>
2025-05-02 14:52:29 +0900

    refactor: implements UserFacade

--- src/main/java/com/servertech/myboard/user/application/controller/UserController.java
+++ src/main/java/com/servertech/myboard/user/application/controller/UserController.java
@@ -4,7 +4,7 @@ import com.servertech.myboard.user.application.dto.request.UserCreateRequest;
 import com.servertech.myboard.user.application.dto.request.UserLoginRequest;
 import com.servertech.myboard.user.application.dto.response.JwtResponse;
 import com.servertech.myboard.user.application.dto.response.UserDetailResponse;
-import com.servertech.myboard.user.application.service.UserService;
+import com.servertech.myboard.user.application.UserFacade;
 import com.servertech.myboard.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,20 +14,20 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
 public class UserController {
-	private final UserService userService;
+	private final UserFacade userFacade;
 
 	@PostMapping("/signup")
 	public void create(@RequestBody UserCreateRequest request) {
-		userService.create(request);
+		userFacade.create(request);
 	}
 
 	@PostMapping("/login")
 	public JwtResponse login(@RequestBody UserLoginRequest request) {
-		return userService.login(request);
+		return userFacade.login(request);
 	}
 
 	@GetMapping("/my")
 	public UserDetailResponse myPage(@AuthenticationPrincipal User user) {
-		return userService.getUserDetail(user);
+		return userFacade.getUserDetail(user);
 	}
 }
