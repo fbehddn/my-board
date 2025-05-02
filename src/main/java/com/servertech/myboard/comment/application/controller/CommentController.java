d3de18b700e9bc497ea87d528527df522cf6c32e
fbehddn <tom0611@naver.com>
2025-05-02 12:46:41 +0900

    refactor: commentController API uri to keep RESTful

--- src/main/java/com/servertech/myboard/comment/application/controller/CommentController.java
+++ src/main/java/com/servertech/myboard/comment/application/controller/CommentController.java
@@ -2,7 +2,6 @@ package com.servertech.myboard.comment.application.controller;
 
 import com.servertech.myboard.comment.application.dto.request.CreateCommentRequest;
 import com.servertech.myboard.comment.application.dto.request.UpdateCommentRequest;
-import com.servertech.myboard.comment.application.dto.response.CommentDetailResponse;
 import com.servertech.myboard.comment.application.dto.response.CommentListResponse;
 import com.servertech.myboard.comment.application.dto.response.CommentResponse;
 import com.servertech.myboard.comment.application.service.CommentService;
@@ -11,18 +10,13 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/comments")
+@RequestMapping("/api")
 public class CommentController {
 	private final CommentService commentService;
 
-	@GetMapping
-	public CommentListResponse getAllComments() {
-		return commentService.findAll();
-	}
-
-	@GetMapping("/{id}")
-	public CommentDetailResponse getComments(@PathVariable Long id) {
-		return commentService.find(id);
+	@GetMapping("/articles/{articleId}/comments")
+	public CommentListResponse getComments(@PathVariable Long articleId) {
+		return commentService.findByArticleId(articleId);
 	}
 
 	@PostMapping("/articles/{articleId}/comments")
@@ -31,12 +25,13 @@ public class CommentController {
 		return commentService.create(articleId, request);
 	}
 
-	@PatchMapping("/{id}")
-	public void updateComment(@PathVariable Long id, @RequestBody UpdateCommentRequest request) {
+	@PatchMapping("/comments/{id}")
+	public void updateComment(@PathVariable Long id,
+							  @RequestBody UpdateCommentRequest request) {
 		commentService.update(id, request);
 	}
 
-	@DeleteMapping("/{id}")
+	@DeleteMapping("/comments/{id}")
 	public void deleteComment(@PathVariable Long id) {
 		commentService.delete(id);
 	}
