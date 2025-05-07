9cb3bb3cd35fe0bcd4d55642519f4ea13abe0065
fbehddn <tom0611@naver.com>
2025-05-07 20:37:37 +0900

    feat: response dto Serializable

--- src/main/java/com/servertech/myboard/article/application/dto/response/ArticleListResponse.java
+++ src/main/java/com/servertech/myboard/article/application/dto/response/ArticleListResponse.java
@@ -2,12 +2,13 @@ package com.servertech.myboard.article.application.dto.response;
 
 import lombok.Builder;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Builder
 public record ArticleListResponse(
 	List<ArticleResponse> articles
-) {
+) implements Serializable {
 	public static ArticleListResponse from(List<ArticleResponse> articles) {
 		return ArticleListResponse.builder()
 			.articles(articles)
