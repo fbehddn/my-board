56a4706ee22948228273811f3da9534a5690c6f7
fbehddn <tom0611@naver.com>
2025-05-22 18:26:13 +0900

    chore: fix test duration

--- load-test/load-test.js
+++ load-test/load-test.js
@@ -15,7 +15,7 @@ export let options = {
             executor: 'constant-arrival-rate',
             rate: 100,
             timeUnit: '1s',
-            duration: '1m',
+            duration: '2m',
             startTime: '10m',      // baseline 종료 시점
             preAllocatedVUs: 100,
             maxVUs: 200,
