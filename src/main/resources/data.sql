ca5610e62f9aaaf03ec738e26cf098aa31ab06c0
fbehddn <tom0611@naver.com>
2025-05-29 17:23:11 +0900

    chore: article data set (100,000)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,26 +1,24 @@
--- User
-INSERT INTO user (id, email, password, username, created_at, updated_at)
-VALUES (1, 'test1@example.com', '{noop}password1', 'UserOne', now(), now()),
-       (2, 'test2@example.com', '{noop}password2', 'UserTwo', now(), now());
+DROP PROCEDURE IF EXISTS seed_articles;
+DELIMITER $$
 
--- Article
-INSERT INTO article (id, title, content, author, user_id, like_count, created_at, updated_at)
-VALUES (1, 'First Article', 'This is the first article content.', 'UserOne', 1, 0, now(), now()),
-       (2, 'Second Article', 'This is the second article content.', 'UserTwo', 2, 0, now(), now()),
-       (3, 'Test Article 3', 'Content for article 3', 'UserOne', 1, 0, now(), now()),
-       (4, 'Test Article 4', 'Content for article 4', 'UserOne', 1, 0, now(), now()),
-       (5, 'Test Article 5', 'Content for article 5', 'UserOne', 1, 0, now(), now()),
-       (6, 'Test Article 6', 'Content for article 6', 'UserOne', 1, 0, now(), now()),
-       (7, 'Test Article 7', 'Content for article 7', 'UserOne', 1, 0, now(), now()),
-       (8, 'Test Article 8', 'Content for article 8', 'UserOne', 1, 0, now(), now()),
-       (9, 'Test Article 9', 'Content for article 9', 'UserOne', 1, 0, now(), now()),
-       (10, 'Test Article 10', 'Content for article 10', 'UserOne', 1, 0, now(), now()),
-       (11, 'Test Article 11', 'Content for article 11', 'UserOne', 1, 0, now(), now()),
-       (12, 'Test Article 12', 'Content for article 12', 'UserOne', 1, 0, now(), now());
+INSERT INTO `user` (id, email, password, username, created_at, updated_at)
+VALUES (1, 'test@example.com', '{bcrypt}pwd', 'enzo', NOW(), NOW());
 
--- Comment
-INSERT
-INTO comment (id, content, author, user_id, article_id, created_at, updated_at)
-VALUES (1, 'First comment on article 1', 'UserTwo', 2, 1, now(), now()),
-       (2, 'Second comment on article 1', 'UserOne', 1, 1, now(), now()),
-       (3, 'Thanks for sharing!', 'UserOne', 1, 2, now(), now());
+CREATE PROCEDURE seed_articles(IN n INT)
+BEGIN
+    DECLARE i INT DEFAULT 0;
+    WHILE i < n DO
+            INSERT INTO article (title,content,author,user_id,created_at,updated_at)
+            VALUES (
+                       CONCAT('테스트 제목 ', i+1),
+                       CONCAT('임의 내용 ',  i+1),
+                       'enzo',
+                       1,
+                       NOW(), NOW()
+                   );
+            SET i = i + 1;
+        END WHILE;
+END$$
+DELIMITER ;
+
+CALL seed_articles(100000);
